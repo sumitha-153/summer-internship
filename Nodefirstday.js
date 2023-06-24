88875b8e0f394101c1a55177a7f02c1fafd4ebc6
sumitha-153 <126260685+sumitha-153@users.noreply.github.com>
2023-06-24 10:31:22 +0545

    Nodefirstday.js

---
+++ Nodefirstday.js
@@ -0,0 +1,17 @@
+console.log("Hello world");
+function sum(num1,num2) {
+    return num1+num2
+}
+console.log(sum(10,20));
+function sub(num1,num2) {
+    return num1-num2
+}
+console.log(sub(10,20));
+function multiply(num1,num2) {
+    return num1*num2
+}
+console.log(multiply(10,20));
+function div(num1,num2) {
+    return num1/num2
+}
+console.log(div(10,20));
