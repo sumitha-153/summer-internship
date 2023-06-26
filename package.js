b6e781c572a2275a277fc639eb40cacebd9c99c7
sumitha-153 <126260685+sumitha-153@users.noreply.github.com>
2023-06-26 10:01:10 +0545

    package.js

---
+++ package.js
@@ -0,0 +1,28 @@
+/*
+npm---->node package manager.React and express are the packages writen by someone.we are just installing and using it.
+to run a express we required 58 packages.The code we installed are present inside the node modules.while creating a packages ,it will give a meta data of the
+packages that we created.
+To install --->npm instal 
+To insatall express package----->npm install express
+*/
+//This the meta data for the package that i had created......
+
+
+{
+  "name": "color-generator",
+  "version": "1.0.0",
+  "description": "To generate random color",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [
+    "learn",
+    "npm"
+  ],
+  "author": "Sumitha",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
